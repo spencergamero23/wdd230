e283878998300f4a51f7a2bf035b3ff504f4cad6
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2024-02-07 14:01:51 -0700

    JS assignment due for class

---
+++ lesson05/script/script.js
@@ -0,0 +1,13 @@
+const input = document.queryselector('#favchap');
+const button = document.queryselector('button')
+const list = document.querySelector('#list')
+
+button.addEventListener('click', function() { 
+    if(input.value !=''){
+        
+    }
+    else{
+        
+    }
+
+});
