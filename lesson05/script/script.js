7e3172a698f1faddf8dec1d547a15a7f61599d02
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2024-02-09 13:25:27 -0700

    Finished the bom page

--- lesson05/script/script.js
+++ lesson05/script/script.js
@@ -1,9 +1,25 @@
-const input = document.queryselector('#favchap');
-const button = document.queryselector('button')
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button')
 const list = document.querySelector('#list')
 
 button.addEventListener('click', function() { 
     if(input.value !=''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        
+        li.append(deleteButton);
+
+        list.append(li);
+        deleteButton.addEventListener('click', function(){
+
+            list.removeChild(li);
+            input.focus();
+            input.value = '';
+
+        });
+        
         
     }
     else{
