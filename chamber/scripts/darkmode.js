fd26118d22735e9d77f5fe2e5171e1688dae2197
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2024-02-16 01:08:46 -0700

    Finished some things for my course home page

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -4,12 +4,15 @@ function toggleTheme() {
     var header = document.querySelector('header');
     var footer = document.querySelector('footer');
     var main = document.querySelector('main');
-    
-    
+    var divp = document.querySelectorAll('p');
+    console.log(divp);
+    divp.forEach((x)=>{
+        x.classList.toggle('dark-mode')
+    });
+
     body.classList.toggle('dark-mode');
     header.classList.toggle('dark-mode');
     footer.classList.toggle('dark-mode');
     main.classList.toggle('dark-mode');
-    divp.classList.toggle('dark-mode');
     
 }
