635c7e21a50127fa317630aa3a9a0d34f76a8921
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2024-03-20 03:13:39 -0600

    Made the mobile view better

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -2,10 +2,3 @@
 document.getElementById("joinButton").addEventListener("click", function() {
     window.location.href = "join.html"; // Replace "another-page.html" with the path to your target HTML page
   });
-// Last Modified info
-function updateLastModified() {
-    var lastModifiedElement = document.getElementById("lastModified");
-    var lastModified = new Date(document.lastModified);
-    lastModifiedElement.textContent = "Last modified: " + lastModified.toLocaleString();
-}
-window.onload = updateLastModified;
---
+++ chamber/scripts/update.js
@@ -0,0 +1,7 @@
+// Last Modified info
+function updateLastModified() {
+    var lastModifiedElement = document.getElementById("lastModified");
+    var lastModified = new Date(document.lastModified);
+    lastModifiedElement.textContent = "Last modified: " + lastModified.toLocaleString();
+}
+window.onload = updateLastModified;
